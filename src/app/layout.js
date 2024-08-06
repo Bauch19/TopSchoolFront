e5fa60d88ee1c6de90f6e439616d94c0d82533b1
Enrique Bouchet <enrique.bouchet19@gmail.com>
2024-08-05 23:45:55 -0600

    Creación de proyecto

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100','200','300','400','500','600','700','800','900'],
+});
+
+export const metadata = {
+  title: "TopSchool",
+  description: "TopSchool primer medio de comunicación escolar",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={`${inter} ${poppins}`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen w-full">
+      <section className="bg-slate-400">
+
+      </section>
+    </main>
+  );
+}
