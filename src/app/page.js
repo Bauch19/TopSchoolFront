f5fc7a09e6d84c29d5c116ab15b8982a99404af5
Enrique Bouchet <enrique.bouchet19@gmail.com>
2024-08-06 23:54:04 -0600

    Cargar imagenes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,34 @@
+'use client';
 import Image from "next/image";
+import dynamic from "next/dynamic";
+const BackGalaxy = dynamic(() => import('@/components/Main/BackGalaxy'));
 
 export default function Home() {
+
   return (
-    <main className="min-h-screen w-full">
-      <section className="wrapper relative">
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
+    <main className="min-h-screen w-full relative">
+      <BackGalaxy />
+      <section className="absolute top-0 z-[5] bg-transparent">
+        <div className="w-full h-full flex flex-col justify-start items-center py-10 bg-transparent">
+          <Image 
+            src={'/titulo-full.png'}
+            width={960}
+            height={540}
+            className="w-1/4 h-auto"
+          />
+          <h2 className="uppercase text-white text-[40px] text-center font-thin w-[330px] leading-[40px] mt-10">
+            El poder del <span className="text-white font-bold">Mindfulness</span> en la infancia
+          </h2>
+          <Image 
+            src={'/boys.png'}
+            width={960}
+            height={540}
+            className="w-1/4 h-auto mt-10"
+          />
+        </div>
+      </section>
+      <section>
+
       </section>
     </main>
   );
---
+++ src/components/Main/BackGalaxy.jsx
@@ -0,0 +1,46 @@
+'use client';
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function BackGalaxy(){
+    return(
+    <section className="w-full h-[100vh] relative">
+    <motion.div
+        inital={{ translateX: '200vw' }}
+        animate={{ translateX: '-200vw' }}
+        transition={{ 
+            repeat: Infinity,
+            repeatType: "loop",
+            duration: 120,
+            ease: "linear",
+        }}
+        className="absolute z-[1] w-full h-full"
+    >
+        <div className="flex w-full">
+            <Image 
+                src={'/galaxy.jpg'}
+                width={1920}
+                height={1080}
+                className="opacity-100 scale-[1] left-0"
+            />
+            <Image 
+                src={'/galaxy.jpg'}
+                width={1920}
+                height={1080}
+                className="opacity-100 scale-[1] right-0 scale-x-[-1]"
+            />
+            <Image 
+                src={'/galaxy.jpg'}
+                width={1920}
+                height={1080}
+                className="opacity-100 scale-[1] right-0"
+            />
+        </div>
+    </motion.div>
+    <section className="wrapper bg-opacity-50 relative z-[2]">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+    </section>
+    </section>
+    )
+}
